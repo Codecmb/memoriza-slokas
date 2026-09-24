cad3fc1f307b6e7ca6c28390849c900ca5a6448a
Codecmb <92221983+Codecmb@users.noreply.github.com>
2026-09-23 20:12:57 -0400

    Add correction tracking and bilingual word-for-word

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='memoriza-slokas-final-v7';
+const CACHE='memoriza-slokas-markers-v9';
 const CORE=['./','./index.html','./verses.json','./audio-map.json','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
